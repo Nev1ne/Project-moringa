3a7b3ba58a1f9aecb5f18d2f53f3ba7a8b83f533
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-04-14 17:30:46 +0300

    edited files

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ let result = document.getElementById("result");
 let getMovie = () =>{
     let movieName = movieNameRef.value;
     let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+
+    //if input field is empty
+if(movieName.length <= 0){
+    result.innerHTML = `<h3 class="msg">Please enter a movie name </h3>`;
+}
+//if input isnt empty
+else{
+    fetch(url).then((resp) => resp.json()).then((data) =>{
+        
+    })
+}
+
 }
