ef34573725a66dfe93b6f569b85900258a861ac3
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-04-15 13:12:01 +0300

    added files

--- index.js
+++ index.js
@@ -16,25 +16,39 @@ if(movieName.length <= 0){
 else{
     fetch(url).then((resp) => resp.json()).then((data) =>{
         //if movie exist in database
-        if (data.Response == "True"){
-            result.innerHTML = `
-            <div class="info">
-            <img src=${data.Poster} class="poster">
-            <div>
-            <h2>${data.Title}</h2>
-            <div class="rating">
-            <img src="">
-            <h4>${data.imdbRating}</h4>
-            </div>
-            <div class="details">
-                <span>${data.Rated}</span>
-                <span>${data.Year}</span>
-                <span>${data.Runtime}</span>
-            </div>
-            <div class="genre"
-            `
+            if (data.Response == "True"){
+                result.innerHTML = `
+                <div class="info">
+                  <img src=${data.Poster} class="poster">
+                <div>
+                   <h2>${data.Title}</h2>
+                      <div class="rating">
+                          <img src="">
+                          <h4>${data.imdbRating}</h4>
+                     </div>
+                    <div class="details">
+                        <span>${data.Rated}</span>
+                        <span>${data.Year}</span>
+                        <span>${data.Runtime}</span>
+                    </div>
+                            <div class="genre">
+                                <div>${data.Genre.split(",").join
+                                ("</div><div>")}</div>
+                            </div>
+                </div>
+                <h3>Plot:</h3>
+                <p>${data.Plot}</p>
+                <h3>Cast:</h3>
+                <p>${data.Actors}</p>
+            `;
         }
+        //if movie doesn't exist in database
+        else{
+            result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+        } 
     })
+
+    //if error occurs
 }
 
 }
